982e6cd25ad201ec1d8931aeb512f4bdb7405e19
gerardoluisbv <gerardoluisbv@gmail.com>
2023-07-06 22:52:49 -0600

    db mysql created(Planetscale and Prisma) and first post from Modal success

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,25 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
+'use client'
 
-export default function Home() {
+import { useStoreModal } from '@/hooks/use-store-modal';
+import { useEffect } from 'react';
+
+const SetupPage = () => {
+
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if(!isOpen){
+      onOpen();
+    }
+  }, [isOpen, onOpen])
+  
+
   return (
     <div className='p-4'>
-    <Button size="default"> Click me! </Button>
-   <h1> Hello Admin Dashboard!!  </h1>
+     Root Page
     </div>
   )
 }
+
+export default SetupPage;
